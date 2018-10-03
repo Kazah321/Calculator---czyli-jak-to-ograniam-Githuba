e63b452f1e65e24a9e52da4fbbbe7649390a60b9
azalesny <adrian.zalesny@gmail.com>
2018-10-03 15:18:49 +0200

    kalkulator.py

--- Documents/My_program/Python/kalkulator.py
+++ Documents/My_program/Python/kalkulator.py
@@ -5,9 +5,14 @@ class Calculator:
 		return a+b
 	def calc_sub(self,a,b):
 		return a-b
-
+	def calc_div(self, a, b):
+		return a/b
+	def calc_mul(self, a, b):
+		return a*b
 #test Calculatora
 	
 x=Calculator()
 print(x.calc_add(4,5))
 print(x.calc_sub(3,2))
+print(x.calc_div(4,2))
+print(x.calc_div(4,2))
