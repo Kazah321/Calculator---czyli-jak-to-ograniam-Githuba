df1b237e663098308415d82d6ed226d4a5d3aaa4
Mateusz B <mateusz.emmanuel47@gmail.com>
2018-10-03 15:37:19 +0200

    kalkulator.py

--- Documents/My_program/Python/kalkulator.py
+++ Documents/My_program/Python/kalkulator.py
@@ -6,8 +6,12 @@ class Calculator:
 	def calc_sub(self,a,b):
 		return a-b
 
+
+	def calc_sq(self,a):
+		return a*a
 #test Calculatora
 	
 x=Calculator()
 print(x.calc_add(4,5))
 print(x.calc_sub(3,2))
+print(x.calc_sq(5))
