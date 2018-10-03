fd2558c13f72f43ea3b1b5a03dfe31d30ae8ef40
Mateusz B <mateusz.emmanuel47@gmail.com>
2018-10-03 15:01:40 +0200

    kalkulator.py

--- Documents/My_program/Python/kalkulator.py
+++ Documents/My_program/Python/kalkulator.py
@@ -3,6 +3,11 @@ class Calculator:
 		pass
 	def calc_add(self,a,b):
 		return a+b
+	def calc_sub(self,a,b):
+		return a-b
+
 #test Calculatora
+	
 x=Calculator()
 print(x.calc_add(4,5))
+print(x.calc_sub(3,2))
